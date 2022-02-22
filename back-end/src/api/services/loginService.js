2d1c1793593b5e14032932d1102a349fdcbde3b4
Demontier <demontier@outlook.com>
2022-02-22 18:46:13 -0300

    criaçao da funcao md5

--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -1,14 +1,17 @@
 const { users } = require('../../database/models');
 const { badRequest } = require('../utils/statusCode');
 const errorConstructor = require('../utils/errorHandling');
+const md5 = require('../utils/md5')
 
 const { genToken } = require('./authService');
 
 const login = async (email, password) => {
+  const hash = md5(password)
+
   const user = await users.findOne({ where: { email } });
   if (!user) throw errorConstructor(badRequest, 'Invalid username or password');
  
-  if (password !== user.dataValues.password) {
+  if (hash !== user.dataValues.password) {
     throw errorConstructor(badRequest, 'Invalid username or password');
   }
 
@@ -16,7 +19,7 @@ const login = async (email, password) => {
 
   const token = genToken(newData);
 
-  return token;
+  return { token };
 };
 
 module.exports = {
--- back-end/src/api/services/usersService.js
+++ back-end/src/api/services/usersService.js
@@ -1,6 +1,7 @@
 const { users } = require('../../database/models');
 const { Conflict, badRequest } = require('../utils/statusCode');
 const errorConstructor = require('../utils/errorHandling');
+const md5 = require('../utils/md5')
 const { userSchema } = require('../utils/schemas');
 
 const createUser = async (data) => {
@@ -9,7 +10,8 @@ const createUser = async (data) => {
 
   const emailExists = await users.findOne({ where: { email: data.email } });
   if (emailExists) throw errorConstructor(Conflict, 'User already registered');
-
+  
+  data.password = md5(data.password);
   await users.create(data);
 
   return null;
