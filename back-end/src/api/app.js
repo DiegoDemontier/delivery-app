256e522f0c981f0f53e76b7d97724a08a854581f
Demontier <demontier@outlook.com>
2022-02-22 16:48:44 -0300

    criaçao do errorMiddleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usersRoutes = require('./router/usersRoutes');
+const errorMiddleware = require('./middleware/errorHandle');
 
 const app = express();
 app.use(express.json());
@@ -7,5 +8,7 @@ app.use(express.json());
 app.use('/user', usersRoutes);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ back-end/src/api/utils/statusCode.js
@@ -0,0 +1,11 @@
+module.exports = {
+  success: 200,
+  created: 201,
+  noContent: 204,
+  badRequest: 400,
+  Unauthorized: 401,
+  notFound: 404,
+  Conflict: 409,
+  unprocessableEntity: 422,
+  serverError: 500,
+};
