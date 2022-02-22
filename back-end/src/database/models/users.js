da9e92710e7703ed12a1562dd3610c3592a5b5ea
Demontier <demontier@outlook.com>
2022-02-22 15:40:38 -0300

    criaçao da tabela users

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,18 @@
+const users = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    'users',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+    },
+  );
+
+  return user;
+};
+
+module.exports = users;
