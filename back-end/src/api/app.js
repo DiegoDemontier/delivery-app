5fe0226731932a8961763b8b909eb08bd4644074
Demontier <demontier@outlook.com>
2022-02-22 16:06:10 -0300

    criaçao da rota user

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const usersRoutes = require('./router/usersRoutes');
 
 const app = express();
+app.use(express.json());
+
+app.use('/user', usersRoutes);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
