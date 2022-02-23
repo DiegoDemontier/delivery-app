fc9221bfa4aafe46c4d91a0a68dad26048295a2d
Carol_Inglethe <carolinainglethe@gmail.com>
2022-02-22 23:24:42 -0300

    frontend - cria rotas e base pagina de login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const usersRoutes = require('./router/usersRoutes');
 const loginRoutes = require('./router/loginRoutes');
 const errorMiddleware = require('./middleware/errorHandle');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/user', usersRoutes);
 app.use('/login', loginRoutes);
