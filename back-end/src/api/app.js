86cf3c69c8376f6774590a81ef57fb4cc0bfb8a6
Demontier <demontier@outlook.com>
2022-02-24 00:21:14 -0300

    feat: static images

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 const usersRoutes = require('./router/usersRoutes');
 const loginRoutes = require('./router/loginRoutes');
 const salesRoutes = require('./router/salesRoutes');
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use('/user', usersRoutes);
 app.use('/login', loginRoutes);
 app.use('/sale', salesRoutes);
+
+app.use('/images', express.static(path.resolve(__dirname, '..', '..', 'public', 'images')));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
