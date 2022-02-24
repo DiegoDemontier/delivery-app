cce47be2ac420eeb10a3fde7c5150aa6e667f428
Demontier <demontier@outlook.com>
2022-02-24 14:32:11 -0300

    refactor: salesService

--- back-end/src/api/services/salesService.js
+++ back-end/src/api/services/salesService.js
@@ -1,13 +1,28 @@
-const { sales } = require('../../database/models');
+const { sales, products, salesProducts } = require('../../database/models');
 const { badRequest } = require('../utils/statusCode');
 const errorConstructor = require('../utils/errorHandling');
 const { saleSchema } = require('../utils/schemas');
 
 const createSale = async (data) => {
+  const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, saleProduct } = data;
+  
   const { error } = saleSchema.validate(data);
   if (error) throw errorConstructor(badRequest, error.message);
+  
+  const arrayProductId = saleProduct.map(({ productId }) => productId);
+  const product = await products.findAll({ where: { id: arrayProductId } });
 
-  await sales.create(data);
+  const sale = await sales.create({
+    userId,
+    sellerId,
+    totalPrice,
+    deliveryAddress,
+    deliveryNumber });
+  await sale.addProducts(product);
+  
+  saleProduct.forEach(async ({ productId, quantity }) => {
+    await salesProducts.update({ quantity }, { where: { productId, saleId: 1 } });
+  });
 
   return null;
 };
