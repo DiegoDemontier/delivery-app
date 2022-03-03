a6f12a1a94a52748536bb8fe09df87fdfdbde83a
Carol_Inglethe <carolinainglethe@gmail.com>
2022-03-03 16:29:34 -0300

    Pagina SellerOrderDetails - Iniciando, arquivo e rota do react

---
+++ front-end/src/pages/SellerOrdersDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SellerOrdersDetails() {
+  return (
+    <div>
+      <h1> Page Seller Order Details (iniciando)</h1>
+    </div>
+  );
+}
+
+export default SellerOrdersDetails;
